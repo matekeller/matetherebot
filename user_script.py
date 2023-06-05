1f5fd9faf910c9e45d9a47eaf8dcb70f390503a0
Lukas Liesenberg <lukas.liesenberg@rwth-aachen.de>
2023-06-05 19:32:20 +0200

    use rwth findme site

--- user_script.py
+++ user_script.py
@@ -1,35 +1,37 @@
-import subprocess
 import os
 import requests
 import json
 from dotenv import find_dotenv, load_dotenv
+from bs4 import BeautifulSoup
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 load_dotenv((find_dotenv()))
 
 aps = [
-    "04:5F:B9:2F:56:2F",
-    "54:8A:BA:CC:80:AF",
-    "04:5F:B9:2F:7D:AF",
-    "04:5F:B9:2F:89:6F",
-    "54:8A:BA:CD:83:CF",
-    "54:8A:BA:CD:83:C0",
-    "14:16:9D:56:B4:C0",
-    "04:5F:B9:2F:78:8F",
-    "54:8A:BA:CB:AE:4F",
-    "54:8A:BA:CD:76:6F",
-    "54:8A:BA:CB:AC:AF",
-    "54:8A:BA:CD:67:4F",
-    "14:16:9D:55:92:60",
-    "04:5F:B9:2F:78:80"
+    "ap-2356-6u07",
+    "ap-2356-6u09",
+    "ap-2356-6u11",
+    # Maybe add more nearby APs if needed
 ]
 
-iwconfig = subprocess.Popen(('iwconfig', 'wlp0s20f3'), stdout=subprocess.PIPE)
+headers = {"Accept": "application/json"}
 
-output = subprocess.check_output(
-    ('grep', '-P', "\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", '-o'), stdin=iwconfig.stdout).strip()
-iwconfig.wait()
-output = output.decode('ascii')
+response = requests.request(
+    method="GET",
+    url="https://findme.rwth-aachen.de/client-info.json",
+    headers=headers,
+    verify=False,
+).json()
 
-if (output in aps):
-    requests.post(os.environ.get('URL') + '/max_present', json={"max_token": os.environ.get('MAXTHERE_TOKEN')})
-    print(output)
+if response.get("html"):
+    soup = BeautifulSoup(response.get("html"), "html.parser")
+
+    ap = soup.find("th", string="Access Point").find_next_sibling("td").string
+
+    if ap in aps:
+        requests.post(
+            os.environ.get("URL") + "/max_present",
+            json={"max_token": os.environ.get("MAXTHERE_TOKEN")},
+        )
