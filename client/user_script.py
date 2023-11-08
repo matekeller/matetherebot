c7df54ac09c60b64820c4100b394de12f3232879
Septatrix <24257556+Septatrix@users.noreply.github.com>
2023-11-08 20:55:11 +0100

    Replace deprecated iwconfig with iw
    Also replace grep subprocess with python regex

--- client/user_script.py
+++ client/user_script.py
@@ -1,35 +1,46 @@
-import subprocess
 import os
+import re
+import subprocess
+
 import requests
-import json
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv((find_dotenv()))
 
-aps = [
-    "04:5F:B9:2F:56:2F",
-    "54:8A:BA:CC:80:AF",
-    "04:5F:B9:2F:7D:AF",
-    "04:5F:B9:2F:89:6F",
-    "54:8A:BA:CD:83:CF",
-    "54:8A:BA:CD:83:C0",
-    "14:16:9D:56:B4:C0",
-    "04:5F:B9:2F:78:8F",
-    "54:8A:BA:CB:AE:4F",
-    "54:8A:BA:CD:76:6F",
-    "54:8A:BA:CB:AC:AF",
-    "54:8A:BA:CD:67:4F",
-    "14:16:9D:55:92:60",
-    "04:5F:B9:2F:78:80",
+# Can be obtained via:
+# `sudo iw dev wlp5s0 scan | jc --iw-scan | jq '.[] | select(.ssid == "eduroam") | .bssid'`
+aps = {
+    "04:5f:b9:2f:56:2f",
+    "54:8a:ba:cc:80:af",
+    "04:5f:b9:2f:7d:af",
+    "04:5f:b9:2f:89:6f",
+    "54:8a:ba:cd:83:cf",
+    "54:8a:ba:cd:83:c0",
+    "14:16:9d:56:b4:c0",
+    "04:5f:b9:2f:78:8f",
+    "54:8a:ba:cb:ae:4f",
+    "54:8a:ba:cd:76:6f",
+    "54:8a:ba:cb:ac:af",
+    "54:8a:ba:cd:67:4f",
+    "14:16:9d:55:92:60",
+    "04:5f:b9:2f:78:80",
     "54:8a:ba:cd:76:60",
+}
 
-iwconfig = subprocess.Popen(('iwconfig', 'wlp0s20f3'), stdout=subprocess.PIPE)
+iw_devices = subprocess.run(["iw", "dev"], check=True, capture_output=True, text=True)
+device_match = re.search(r"Interface (?P<device_name>\w+)", iw_devices.stdout)
 
-output = subprocess.check_output(
-    ('grep', '-P', "\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", '-o'), stdin=iwconfig.stdout).strip()
-iwconfig.wait()
-output = output.decode('ascii')
+iw_link = subprocess.run(
+    ["iw", "dev", device_match["device_name"], "link"],
+    check=True,
+    capture_output=True,
+    text=True,
+)
+bssid_match = re.search(r"Connected to (?P<bssid>[\w:]+)", iw_link.stdout)
 
-if (output in aps):
-    requests.post(os.environ.get('URL') + '/max_present', json={"max_token": os.environ.get('MAXTHERE_TOKEN')})
-    print(output)
+if bssid_match["bssid"] in aps:
+    print(bssid_match["bssid"])
+    requests.post(
+        os.environ["URL"] + "/max_present",
+        json={"max_token": os.environ["MAXTHERE_TOKEN"]},
+    )
