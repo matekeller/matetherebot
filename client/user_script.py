1691dc6e9979b7085c848431872f5e974e96c143
Gameheld <lukas.liesenberg@web.de>
2025-01-10 10:46:50 +0100

    update BSSIDs

--- client/user_script.py
+++ client/user_script.py
@@ -25,8 +25,26 @@ aps = {
     "14:16:9d:55:92:60",
     "04:5f:b9:2f:78:80",
     "54:8a:ba:cd:76:60",
+    "04:5f:b9:2f:7d:ad",
+    "54:8a:ba:cc:80:a2",
+    "54:8a:ba:cd:76:6d",
+    "04:5f:b9:2e:d2:8d",
+    "04:5f:b9:2f:78:8d",
+    "04:5f:b9:2f:89:6d",
+    "04:5f:b9:2f:78:82",
+    "04:5f:b9:2f:56:22",
+    "04:5f:b9:2f:56:2d",
+    "54:8a:ba:cc:80:ad",
+    "14:16:9d:55:7b:22",
+    "54:8a:ba:cd:67:4d",
+    "04:5f:b9:2f:56:2e",
+    "54:8a:ba:cc:80:a1",
+    "04:5f:b9:2f:56:21",
+    "04:5f:b9:2f:7d:ae",
+    "04:5f:b9:2f:78:81",
+    "54:8a:ba:cd:67:4e",
+    "04:5f:b9:2f:89:6e"
 }
-
 iw_devices = subprocess.run(["iw", "dev"], check=True, capture_output=True, text=True)
 device_match = re.search(r"Interface (?P<device_name>\w+)", iw_devices.stdout)
 
@@ -40,7 +58,8 @@ bssid_match = re.search(r"Connected to (?P<bssid>[\w:]+)", iw_link.stdout)
 
 if bssid_match["bssid"] in aps:
     print(bssid_match["bssid"])
-    requests.post(
+    request = requests.post(
         os.environ["URL"] + "/max_present",
         json={"max_token": os.environ["MAXTHERE_TOKEN"]},
     )
+    print("Resp:", request.status_code, request.reason)
